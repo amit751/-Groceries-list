74edb41f7a08872387766d65d8ddc934f8f17f72
amit <amitoosh.mor@gmail.com>
2021-03-16 17:06:53 +0200

    first comit-working alone

---
+++ groceries/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GrouceryList from "./components/GroceryItem"
+function App() {
+  return (
+    <div className="App">
+      < GrouceryList />
+
+    </div>
+  );
+}
+
+export default App;
